56cc04c65a73d539ffae3a1b06e1e3bf8fd3dc1e
SalFragoso <salfragosm@gmail.com>
2026-03-06 23:32:40 -0500

    Added User Entity

---
+++ backend/src/main/java/com/backend/backend/model/User.java
@@ -0,0 +1,65 @@
+package com.backend.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String username, String password, String email) {
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
